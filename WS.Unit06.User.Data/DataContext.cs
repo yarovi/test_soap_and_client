b9b60999bc36ae5ba975e0de89166040085adf4e
yasmani <tas185@gmail.com>
2024-02-10 18:02:40 +0100

    se mejoro la busqueda del archivo .sqlite cuando es un compilado

--- WS.Unit06.User.Data/DataContext.cs
+++ WS.Unit06.User.Data/DataContext.cs
@@ -8,8 +8,10 @@ namespace WS.Unit06.User.Data
 		protected override void OnConfiguring(DbContextOptionsBuilder
 optionsBuilder)
 		{
-			optionsBuilder.UseSqlite("Data Source=data/database.db");
-		}
+            //optionsBuilder.UseSqlite("Data Source=data/database.db");
+            var dbPath = Path.Combine(AppContext.BaseDirectory, "data", "database.sqlite");
+            optionsBuilder.UseSqlite($"Data Source={dbPath}");
+        }
 		public DbSet<Users> Users { get; set; }
 	}
 }
