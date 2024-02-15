032696624579411746b726d4ae173c6f5846cc38
yasmani <tas185@gmail.com>
2024-02-15 17:41:00 +0100

    responseCustom

---
+++ WS.Unit06.User.Application/util/ResponseCustom.cs
@@ -0,0 +1,8 @@
+﻿namespace WS.Unit06.User.Application.util
+{
+    public class ResponseCustom
+    {
+        public int code {  get; set; }
+        public string messageCustom { get; set; }
+    }
+}
