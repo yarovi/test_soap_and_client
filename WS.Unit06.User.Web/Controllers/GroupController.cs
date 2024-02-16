a6a5def8f5403bad6bbe325aa2b865fbb0a85003
yasmani <tas185@gmail.com>
2024-02-16 15:04:31 +0100

    se agrego  la vista de gastos

---
+++ WS.Unit06.User.Web/Controllers/GroupController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Mvc.Formulario.Gastos.Controllers
+{
+    public class GroupController :Controller
+    {
+        public IActionResult createGroup()
+        {
+            return View();
+        }
+
+        public IActionResult groupUser()
+        {
+            return View();
+        }
+        public IActionResult expenseGroup() {
+        return View();
+        }
+    }
+}
---
+++ WS.Unit06.User.Web/Util/SecurityHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace WS.Unit06.User.Web.Util
+{
+	public class SecurityHelper
+	{
+		public static string HashPassword(string password)
+		{
+			return BCrypt.Net.BCrypt.HashPassword(password);
+		}
+
+		public static bool VerifyPassword(string password, string hashedPassword)
+		{
+			return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+		}
+	}
+}
